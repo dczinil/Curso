63955f2b20057cbef7846cb01e75248b144a14ce
dczinil@tython.ub <dczinil@gmail.com>
2022-11-01 07:29:15 -0600

    python os path libraries

---
+++ lib_os_path/ospath.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import platform
+
+
+print(os.path)
+print(os.name)
+print(sys.platform)
+print(os.getcwd())
+# os.chdir("..")
+# print(os.getcwd())
+# print(os.listdir())
+# os.mkdir("Nuevo_directorio")
+# os.chdir("Nuevo_directorio")
+
+print(sys.modules)
